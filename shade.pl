6b4f2ec76b62a272c0a76613c008bbbcbac46575
code <code@847b6420-b38f-4084-975a-97ed49872a38>
2009-06-01 22:13:02 +0000

    adicionado msg, notice, admin, ignore. dos requisitos nao fazemos deliberadamente: HELP, LIST, AWAY, WHOIS e NOTIFY por nao fazerem sentido para o bot (talvez o notify para os admins?). O ACTION ainda nao ta a dar
    git-svn-id: svn+ssh://94.132.161.125/feup/rcom/shade@1399 847b6420-b38f-4084-975a-97ed49872a38

--- shade.pl
+++ shade.pl
@@ -5,7 +5,7 @@
 :- dynamic irc_response/3.
 :- dynamic irc_admin_response/3.
 :- dynamic irc_send/1.
-
+:- dynamic tmp_admin/1.
 /*
  * Debug stuff:
  */
@@ -143,6 +143,7 @@ irc_receive(_):- write('Unknown message'),nl.
  */
 is_admin('apinto').
 is_admin('jaguarandi').
+is_admin(Nick):-tmp_admin(Nick).
 bot_control(['quit']):-!,write('quitting'),nl,irc_send(['QUIT :I am seeing the WHITE ROOMS 8D']),irc_disconnect.
 bot_control(['nick', Nick]):-!,irc_nick(Nick).
 bot_control(['nick', Nick, Pass]):-!,nick_auth(Nick, Pass).
@@ -154,9 +155,16 @@ bot_control(['kick', Nick, Channel | []]):-!,irc_send(['KICK',Channel,Nick,':DAR
 bot_control(['kick', Nick, Channel | Msg]):-!,clean_msg(NMsg, Msg),append(['KICK',Channel,Nick], NMsg, Final),!,irc_send(Final).
 bot_control(['topic', Channel | Topic]):-!,clean_msg(NTopic, Topic),!,irc_send(['TOPIC',Channel|NTopic]).
 bot_control(['me' | Msg]):-!,clean_msg(NMsg, Msg),append([':\ACTION'|NMsg],['\'],Final),!,irc_send(Final).
+bot_control(['op', Nick, Channel]):-!,irc_send(['MODE', Channel,'+o',Nick]).
+bot_control(['deop', Nick, Channel]):-!,irc_send(['MODE', Channel,'-o',Nick]).
+bot_control(['voice', Nick, Channel]):-!,irc_send(['MODE', Channel,'+v',Nick]).
+bot_control(['devoice', Nick, Channel]):-!,irc_send(['MODE', Channel,'-v',Nick]).
+bot_control(['msg', Dest | Msg]):-!,clean_msg(NMsg, Msg),irc_send(['PRIVMSG', Dest | NMsg]).
+bot_control(['notice', Dest | Msg]):-!,clean_msg(NMsg, Msg),irc_send(['NOTICE', Dest | NMsg]).
+bot_control(['admin', Nick]):-!,asserta( tmp_admin(Nick) ).
+bot_control(['ignore', Nick]):-!,retract( tmp_admin(Nick) ).
 /*Request: PRIVMSG #booka_shade :\001ACTION wonders what's the real implementation of me\001*/
 /*bot_control(L):-write('Comando invalido: |'),write(L),write('|'),nl. */
-
 
 /*
  * Defines responses to given inputs
